191b481d7ccc39ecc536d297f2e6fb1c88c83e22
kodokok <sabdo.tri.atmojo@gmail.com>
2019-09-23 21:36:08 +0700

    Update KomptensiAuditorTableSeeder.php

--- database/seeds/KomptensiAuditorTableSeeder.php
+++ database/seeds/KomptensiAuditorTableSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\KompetensiAuditor;
+use App\User;
 use Illuminate\Database\Seeder;
 
 class KomptensiAuditorTableSeeder extends Seeder
@@ -11,6 +13,9 @@ class KomptensiAuditorTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $user = User::findOrFail(1);
+        $user2 = User::findOrFail(2);
+
+        KompetensiAuditor::create(['user_id' => $user->id, '']);
     }
 }
