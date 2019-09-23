7a6491be4cbc48e1de8a7a8583b3819e6c46a9f1
kodokok <sabdo.tri.atmojo@gmail.com>
2019-09-23 21:37:53 +0700

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,8 +20,8 @@ class CreateUsersTable extends Migration
             $table->string('address')->nullable();
             $table->string('phone')->nullable();
             $table->string('image')->nullable();
-            $table->unsignedBigInteger('department')->nullable();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('pendidikan')->nullable();
+            $table->unsignedBigInteger('department_id')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
