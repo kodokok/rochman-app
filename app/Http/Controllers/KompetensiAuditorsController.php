44b78921573630eb6d1a9bacaf1f0f65069c20d0
kodokok <sabdo.tri.atmojo@gmail.com>
2019-09-23 22:43:19 +0700

    Update KompetensiAuditorsController.php

--- app/Http/Controllers/KompetensiAuditorsController.php
+++ app/Http/Controllers/KompetensiAuditorsController.php
@@ -2,9 +2,64 @@
 
 namespace App\Http\Controllers;
 
+use App\KompetensiAuditor;
+use App\User;
 use Illuminate\Http\Request;
 
 class KompetensiAuditorsController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('kompetensi.index');
+    }
+
+    public function create()
+    {
+        $model = new KompetensiAuditor();
+        $auditor = User::roles(['auditor','auditor_lead'])->all();;
+        return view('kompetensi.form', compact(['model','auditor']));
+    }
+
+    public function store(Request $request)
+    {
+
+    }
+
+    public function edit(Departement $departement)
+    {
+
+    }
+
+    public function update(Request $request, Departement $departement)
+    {
+
+    }
+
+    public function destroy(Departement $departement)
+    {
+
+    }
+
+    public function dataTable()
+    {
+        $model = KompetensiAuditor::all();
+        // dd(Departement::find(1)->user->name);
+
+        return DataTables::of($model)
+            ->addColumn('auditor', function($model) {
+                $user = KompetensiAuditor::find($model->id)->user;
+                return $user ? $user->name : '';
+            })
+            ->addColumn('action', function ($model) {
+                return view('layouts.partials._action', [
+                    'model' => $model,
+                    'url_show' => null,
+                    'url_edit' => route('kompetensi.edit', $model->id),
+                    'url_destroy' => route('kompetensi.destroy', $model->id),
+                ]);
+            })
+            ->addIndexColumn()
+            ->rawColumns(['action'])
+            ->make(true);
+    }
 }
