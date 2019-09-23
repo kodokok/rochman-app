2f30484610fd4e3b274ad1986acc65c549d24682
kodokok <sabdo.tri.atmojo@gmail.com>
2019-09-23 22:39:31 +0700

    Update form.blade.php
    add form view komptensi

--- resources/views/kompetensi/form.blade.php
+++ resources/views/kompetensi/form.blade.php
@@ -12,20 +12,33 @@
     </div>
 </div>
 <div class="form-group row">
-    <label for="user_id" class="col-sm-2 col-form-label">Auditor</label>
+    <label for="pendidikan" class="col-sm-2 col-form-label">Pendidikan</label>
     <div class="col-sm-10">
-        {!! Form::text('name', null, ['class' => 'form-control', 'id' => 'name']) !!}
-        <div id="error-name" class="invalid-feedback"></div>
+        {!! Form::text('pendidikan', null, ['class' => 'form-control', 'id' => 'pendidikan']) !!}
+        <div id="error-pendidikan" class="invalid-feedback"></div>
     </div>
 </div>
 <div class="form-group row">
-    <label for="lokasi" class="col-sm-2 col-form-label">Lokasi</label>
+    <label for="masa_kerja" class="col-sm-2 col-form-label">Masa Kerja</label>
     <div class="col-sm-10">
-        {!! Form::text('lokasi', null, ['class' => 'form-control', 'id' => 'lokasi']) !!}
-        <div id="error-lokasi" class="invalid-feedback"></div>
+        {!! Form::text('masa_kerja', null, ['class' => 'form-control', 'id' => 'masa_kerja']) !!}
+        <div id="error-masa_kerja" class="invalid-feedback"></div>
     </div>
 </div>
-
+<div class="form-group row">
+    <label for="pelatihan" class="col-sm-2 col-form-label">Pelatihan</label>
+    <div class="col-sm-10">
+        {!! Form::text('masa_kerja', null, ['class' => 'form-control', 'id' => 'masa_kerja']) !!}
+        <div id="error-masa_kerja" class="invalid-feedback"></div>
+    </div>
+</div>
+<div class="form-group row">
+    <label for="tanggal_pelatihan" class="col-sm-2 col-form-label">Tanggal Pelatihan</label>
+    <div class="col-sm-10">
+        {!! Form::text('tanggal_pelatihan', null, ['class' => 'form-control', 'id' => 'tanggal_pelatihan']) !!}
+        <div id="error-tanggal_pelatihan" class="invalid-feedback"></div>
+    </div>
+</div>
 
 
 {!! Form::close() !!}
