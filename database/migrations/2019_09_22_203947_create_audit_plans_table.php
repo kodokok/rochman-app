f07571122ddae858a3b0bcc5f4b5bc5240defac1
kodokok <sabdo.tri.atmojo@gmail.com>
2019-09-24 19:38:46 +0700

    Create 2019_09_22_203947_create_audit_plans_table.php

---
+++ database/migrations/2019_09_22_203947_create_audit_plans_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAuditPlansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('audit_plans', function (Blueprint $table) {
+            $table->bigIncrements('id')->unsigned();
+            $table->string('objektif_audit')->nullable();
+            $table->string('klausul')->nullable();
+            $table->unsignedBigInteger('departement');
+            $table->unsignedTinyInteger('konfirmasi_kadept')->nullable();
+            $table->unsignedBigInteger('auditee');
+            $table->unsignedBigInteger('auditor');
+            $table->unsignedBigInteger('lead_auditor');
+            $table->date('tanggal')->nullable();
+            $table->time('waktu')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('audit_plans');
+    }
+}
