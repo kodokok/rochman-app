cd5f11121cc2bb6bfb03f798b58ab94d0e31c1ce
kodokok <sabdo.tri.atmojo@gmail.com>
2019-09-24 19:57:04 +0700

    Update AuditPlan.php

--- app/AuditPlan.php
+++ app/AuditPlan.php
@@ -3,9 +3,14 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\AuditNote;
 
 class AuditPlan extends Model
 {
     protected $guarded = [];
 
+    public function auditNotes()
+    {
+        return $this->hasMany(AuditNote::class);
+    }
 }
