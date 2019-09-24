4a638a41f919637161f6a929e999b7310d64b3e8
kodokok <sabdo.tri.atmojo@gmail.com>
2019-09-25 00:09:14 +0700

    Update scripts.blade.php

--- resources/views/layouts/partials/scripts.blade.php
+++ resources/views/layouts/partials/scripts.blade.php
@@ -4,8 +4,6 @@
 <script src="{{ asset('plugins/jquery/jquery.min.js') }}"></script>
 <!-- Bootstrap -->
 <script src="{{ asset('plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-<!-- AdminLTE -->
-<script src="{{ asset('plugins/adminlte/js/adminlte.js') }}"></script>
 <!-- dataTables -->
 <script src="{{ asset('plugins/datatables/jquery.dataTables.js') }}"></script>
 <script src="{{ asset('plugins/datatables/dataTables.bootstrap4.js') }}"></script>
@@ -17,5 +15,9 @@
 <script src="{{ asset('plugins/moment/moment.min.js') }}"></script>
 <!-- sweetalert2 -->
 <script src="{{ asset('plugins/sweetalert2/sweetalert2.min.js') }}"></script>
+<!-- FastClick -->
+<script src="{{ asset('plugins/fastclick/fastclick.js')}} "></script>
+<!-- AdminLTE -->
+<script src="{{ asset('plugins/adminlte/js/adminlte.min.js') }}"></script>
 <!-- Custom JS -->
-<script src="{{ asset('js/app.js') }}"></script>
+{{-- <script src="{{ asset('js/app.js') }}"></script> --}}
