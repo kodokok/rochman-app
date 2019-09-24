b8314968fc5b77c2d8813ef59f833b7a7d6949f1
kodokok <sabdo.tri.atmojo@gmail.com>
2019-09-24 19:49:31 +0700

    Update AuditNote.php

--- app/AuditNote.php
+++ app/AuditNote.php
@@ -3,8 +3,19 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\User;
+use App\AuditPlan;
 
 class AuditNote extends Model
 {
-    //
+    protected $guarded = [];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function auditPlan()
+    {
+        return $this->belongsTo(AuditPlan::class);
+    }
 }
