cf5376942f819a4362e36a6ed953814721f3fd59
kodokok <sabdo.tri.atmojo@gmail.com>
2019-09-25 00:09:10 +0700

    Update sidebar.blade.php

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -15,8 +15,8 @@
                 <!-- Add icons to the links using the .nav-icon class
                    with font-awesome or any other icon font library -->
                 <li class="nav-item">
-                    <a href="{{ route('app') }}" class="nav-link {{ active(['app']) }}">
-                        <i class="nav-icon fas fa-tacappter-alt"></i>
+                    <a href="{{ route('app') }}" class="nav-link {{ active(['/']) }}">
+                        <i class="nav-icon fas fa-tachometer-alt"></i>
                         <p>Dashboard</p>
                     </a>
                 </li>
