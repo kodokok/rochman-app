72bb86dc6cca84b64df0690b71db4a54d7638ee6
kodokok <sabdo.tri.atmojo@gmail.com>
2019-09-24 20:08:54 +0700

    Update index.blade.php

--- resources/views/audit_plans/index.blade.php
+++ resources/views/audit_plans/index.blade.php
@@ -11,7 +11,7 @@
             <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-right">
                     <li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
-                    <li class="breadcrumb-item active">Departements</li>
+                    <li class="breadcrumb-item active">Audit Plan</li>
                 </ol>
             </div>
         </div>
