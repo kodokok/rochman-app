edf9d7eb772d477a9027fefec9fd6fabf30200b6
kodokok <sabdo.tri.atmojo@gmail.com>
2019-09-24 20:52:41 +0700

    Update AuditPlanTableSeeder.php

--- database/seeds/AuditPlanTableSeeder.php
+++ database/seeds/AuditPlanTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\AuditPlan;
+use App\Departement;
 use App\User;
 use Illuminate\Database\Seeder;
 
@@ -13,16 +14,54 @@ class AuditPlanTableSeeder extends Seeder
      */
     public function run()
     {
+        $admin = User::findOrFail(1);
         $auditee = User::findOrFail(2);
         $auditor = User::findOrFail(3);
         $auditor_leader = User::findOrFail(4);
+        $dept1 = Departement::findOrFail(1);
+        $dept2 = Departement::findOrFail(2);
+        $dept3 = Departement::findOrFail(3);
+
         AuditPlan::create([
-            'objektif_audit' => 'Review SOP',
+            'objektif_audit' => 'Review SOP ' . $dept1->name,
             'klausul' => 'ISO 1009',
-            'departement_id' => 1,
+            'departement_id' => $dept1->id,
+            'konfirmasi_kadept' => 1,
+            'auditee' => $admin->id,
+            'auditor' => $auditor->id,
+            'auditor_leader' => $auditor_leader->id,
+            'tanggal' => date('Y-m-d'),
+            'waktu' => date('H:i:s'),
+        ]);
+        AuditPlan::create([
+            'objektif_audit' => 'Review SOP ' . $dept2->name,
+            'klausul' => 'ISO 1009',
+            'departement_id' => $dept2->id,
             'konfirmasi_kadept' => 0,
             'auditee' => $auditee->id,
             'auditor' => $auditor->id,
+            'auditor_leader' => $auditor_leader->id,
+            'tanggal' => date('Y-m-d'),
+            'waktu' => date('H:i:s'),
+        ]);
+        AuditPlan::create([
+            'objektif_audit' => 'Review SOP ' . $dept3->name,
+            'klausul' => 'ISO 1009',
+            'departement_id' => $dept3->id,
+            'konfirmasi_kadept' => 0,
+            'auditee' => $auditee->id,
+            'auditor' => $auditor->id,
+            'auditor_leader' => $auditor_leader->id,
+            'tanggal' => date('Y-m-d'),
+            'waktu' => date('H:i:s'),
+        ]);
+        AuditPlan::create([
+            'objektif_audit' => 'Review SOP ' . $dept3->name,
+            'klausul' => 'ISO 1009',
+            'departement_id' => $dept3->id,
+            'konfirmasi_kadept' => 0,
+            'auditee' => $auditee->id,
+            'auditor' => $admin->id,
             'auditor_leader' => $auditor_leader->id,
             'tanggal' => date('Y-m-d'),
             'waktu' => date('H:i:s'),
