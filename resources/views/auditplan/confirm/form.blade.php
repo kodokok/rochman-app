431b29e51dcca5fd3d452c424a782eb1251d2fc9
kodokok <sabdo.tri.atmojo@gmail.com>
2019-09-26 03:48:07 +0700

    Update form.blade.php

--- resources/views/auditplan/confirm/form.blade.php
+++ resources/views/auditplan/confirm/form.blade.php
@@ -7,7 +7,7 @@
         'method' => 'PUT',
         'autocomplete' => 'off'
         ]) !!}
-        <div class="card card-primary">
+        <div class="card card-secondary">
             <div class="card-header">
                 <h3 class="card-title">General</h3>
                 <div class="card-tools">
@@ -59,7 +59,7 @@
         <!-- /.card -->
     </div>
     <div class="col-md-6">
-        <div class="card card-primary">
+        <div class="card card-secondary">
             <div class="card-header">
                 <h3 class="card-title">Schedule</h3>
                 <div class="card-tools">
@@ -86,12 +86,85 @@
             </div>
             <!-- /.card-body -->
         </div>
+        <div class="card card-primary">
+            <div class="card-header">
+                <h3 class="card-title">Change Schedule</h3>
+                <div class="card-tools">
+                    <div class="card-tools">
+                        <button type="button" class="btn btn-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse">
+                            <i class="fas fa-minus"></i></button>
+                    </div>
+                </div>
+            </div>
+            <div class="card-body">
+                <div class="form-group row">
+                    <div class="col-sm-5">
+                        <div class="form-group">
+                            <label for="tanggal">Tanggal</label>
+                            <div class="input-group date" id="datetimepicker4" data-target-input="nearest">
+                                <input id="tanggal" name="tanggal" type="text"
+                                    class="form-control datetimepicker-input {{ $errors->has('tanggal') ? ' is-invalid': '' }}"
+                                    data-target="#datetimepicker4"
+                                    value="{{ $model->exists ? $model->tanggal : old('tanggal') }}"
+                                />
+                                <div class="input-group-append" data-target="#datetimepicker4" data-toggle="datetimepicker">
+                                    <div class="input-group-text"><i class="fa fa-calendar"></i></div>
+                                </div>
+                                <div id="error-tanggal" class="invalid-feedback">{{ $errors->first('tanggal') }}</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-5">
+                        <div class="form-group">
+                            <label for="waktu">Waktu</label>
+                            <div class="input-group date" id="datetimepicker3" data-target-input="nearest">
+                                <input id="waktu" name="waktu" type="text"
+                                    class="form-control datetimepicker-input {{ $errors->has('waktu') ? ' is-invalid': '' }}"
+                                    data-target="#datetimepicker3"
+                                    value="{{ $model->exists ? $model->waktu : old('waktu') }}"
+                                />
+                                <div class="input-group-append" data-target="#datetimepicker3" data-toggle="datetimepicker">
+                                    <div class="input-group-text"><i class="fas fa-clock"></i></div>
+                                </div>
+                                <div id="error-waktu" class="invalid-feedback">{{ $errors->first('waktu') }}</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-2">
+                        <div class="form-group">
+                            <label for="">&nbsp;</label>
+                            <button class="form-control btn btn-info">Change</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- /.card-body -->
+        </div>
+        <div class="card card-primary">
+            <div class="card-header">
+                <h3 class="card-title">Remarks</h3>
+                <div class="card-tools">
+                    <div class="card-tools">
+                        <button type="button" class="btn btn-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse">
+                            <i class="fas fa-minus"></i></button>
+                    </div>
+                </div>
+            </div>
+            <div class="card-body">
+                <div class="form-group">
+                    <label for="remarks">Remarks</label>
+                    {!! Form::textarea('remarks', null, ['class' => 'form-control', 'id' => 'remarks', 'rows' => 3]) !!}
+                </div>
+            </div>
+            <!-- /.card-body -->
+        </div>
     </div>
 </div>
 <div class="row">
     <div class="col-12">
         <a href="{{ route('auditplan.index') }}" class="btn btn-secondary">Cancel</a>
-        <input type="submit" value="Confirm" class="btn btn-success float-right">
+        <input type="submit" value="Approve" class="btn btn-success float-right">
+        <input type="submit" value="Reject" class="btn btn-danger float-right mr-2">
     </div>
 </div>
 {!! Form::close() !!}
