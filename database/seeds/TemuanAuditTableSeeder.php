b5f3731e994f4e546fb09aa11d24ffe40e6f072a
kodokok <sabdo.tri.atmojo@gmail.com>
2019-09-26 21:39:35 +0700

    Update TemuanAuditTableSeeder.php
    update dummy data temuan

--- database/seeds/TemuanAuditTableSeeder.php
+++ database/seeds/TemuanAuditTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\TemuanAudit;
 use Illuminate\Database\Seeder;
 
 class TemuanAuditTableSeeder extends Seeder
@@ -11,6 +12,62 @@ class TemuanAuditTableSeeder extends Seeder
      */
     public function run()
     {
+        TemuanAudit::create([
+            'auditplan_id' => 1,
+            'status' => 0,
+            'ketidaksesuaian' => 'auditplan 1, ini tidak sesuai ini pertama',
+            'akar_masalah' => 'auditplan 1, ini akar masalah pertama',
+            'tindakan_perbaikan' => 'auditplan 1, tindakan perbaikan pertama',
+            'duedate_pebaikan' => date('Y-m-d'),
+            'tindakan_pencegahan' => 'auditplan 1, tindakan penceganan pertama',
+            'duedate_pencegahan' => date('Y-m-d'),
+            'approve_dept' => 0,
+            'approve_auditee' => 0,
+            'approve_auditor' => 0,
+            'approve_auditor_lead' => 0,
+        ]);
+        TemuanAudit::create([
+            'auditplan_id' => 1,
+            'status' => 1,
+            'ketidaksesuaian' => 'auditplan 1, ini tidak sesuai ini kedua',
+            'akar_masalah' => 'auditplan 1, ini akar masalah kedua',
+            'tindakan_perbaikan' => 'auditplan 1,tindakan perbaikan kedua',
+            'duedate_pebaikan' => date('Y-m-d'),
+            'tindakan_pencegahan' => 'auditplan 1,tindakan penceganan kedua',
+            'duedate_pencegahan' => date('Y-m-d'),
+            'approve_dept' => 1,
+            'approve_auditee' => 1,
+            'approve_auditor' => 1,
+            'approve_auditor_lead' => 1,
+        ]);
 
+        TemuanAudit::create([
+            'auditplan_id' => 2,
+            'status' => 1,
+            'ketidaksesuaian' => 'auditplan 2,ini tidak sesuai ini pertama',
+            'akar_masalah' => 'auditplan 2,ini akar masalah pertama',
+            'tindakan_perbaikan' => 'auditplan 2,tindakan perbaikan pertama',
+            'duedate_pebaikan' => date('Y-m-d'),
+            'tindakan_pencegahan' => 'auditplan 2,tindakan penceganan pertama',
+            'duedate_pencegahan' => date('Y-m-d'),
+            'approve_dept' => 1,
+            'approve_auditee' => 1,
+            'approve_auditor' => 0,
+            'approve_auditor_lead' => 0,
+        ]);
+        TemuanAudit::create([
+            'auditplan_id' => 2,
+            'status' => 0,
+            'ketidaksesuaian' => 'auditplan 2,ini tidak sesuai ini kedua',
+            'akar_masalah' => 'auditplan 2,ini akar masalah pertama',
+            'tindakan_perbaikan' => 'auditplan 2,tindakan perbaikan kedua',
+            'duedate_pebaikan' => date('Y-m-d'),
+            'tindakan_pencegahan' => 'auditplan 2,tindakan penceganan kedua',
+            'duedate_pencegahan' => date('Y-m-d'),
+            'approve_dept' => 0,
+            'approve_auditee' => 0,
+            'approve_auditor' => 0,
+            'approve_auditor_lead' => 0,
+        ]);
     }
 }
