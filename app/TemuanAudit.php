05ea06ac4d84e0ec51381c690cb1423727a2dea1
kodokok <sabdo.tri.atmojo@gmail.com>
2019-09-29 00:12:47 +0700

    Update TemuanAudit.php
    add attribute date

--- app/TemuanAudit.php
+++ app/TemuanAudit.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use App\AuditPlan;
+use Carbon\Carbon;
 use Illuminate\Support\Arr;
 
 class TemuanAudit extends Model
@@ -15,6 +16,18 @@ class TemuanAudit extends Model
     public function getStatusAttribute($value)
     {
         return Arr::get($this->statusList, $value);
+    }
+
+    public function getDuedatePerbaikanAttribute($value)
+    {
+        // dd($value);
+        return Carbon::parse($value)->format('m-d-Y');
+    }
+
+    public function getDuedatePencegahanAttribute($value)
+    {
+        // dd($value);
+        return Carbon::parse($value)->format('m-d-Y');
     }
 
     public function audit_plan()
