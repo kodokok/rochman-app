b4cdf1324e32bbc035eaacabf4bdb6b48541b026
kodokok <sabdo.tri.atmojo@gmail.com>
2019-09-29 20:36:22 +0700

    Update auditplan.blade.php

--- resources/views/dashboard/list/auditplan.blade.php
+++ resources/views/dashboard/list/auditplan.blade.php
@@ -46,16 +46,7 @@
     </div>
     <!-- /.card-body -->
     <div class="card-footer clearfix">
-        {{-- @if (isset($auditplans)) --}}
-            {{ $auditplans->links() }}
-        {{-- @endif --}}
-        {{-- <ul class="pagination pagination-sm m-0 float-right">
-            <li class="page-item"><a class="page-link" href="#">«</a></li>
-            <li class="page-item"><a class="page-link" href="#">1</a></li>
-            <li class="page-item"><a class="page-link" href="#">2</a></li>
-            <li class="page-item"><a class="page-link" href="#">3</a></li>
-            <li class="page-item"><a class="page-link" href="#">»</a></li>
-        </ul> --}}
+        {{ $auditplans->links() }}
     </div>
     <!-- /.card-footer -->
 </div>
