d603d431e1b29a1ca8d3a53a18d1705e7e477a0b
kodokok <sabdo.tri.atmojo@gmail.com>
2019-10-02 21:47:38 +0700

    Update breadcrumbs.blade.php
    uppercase breadcrumb text

--- resources/views/partials/breadcrumbs.blade.php
+++ resources/views/partials/breadcrumbs.blade.php
@@ -1,19 +1,14 @@
 @if (isset($breadcrumbs))
     @if (count($breadcrumbs))
         <ol class="breadcrumb breadcrumb-right-arrow">
-        {{-- <li class="nav-item"> --}}
             @foreach ($breadcrumbs as $breadcrumb)
-
                 @if ($breadcrumb->url && !$loop->last)
-                    <li class="breadcrumb-item"><a href="{{ $breadcrumb->url }}">{{ $breadcrumb->title }}</a></li>
-                    {{-- <a class="nav-link" href="{{ $breadcrumb->url }}">{{ $breadcrumb->title }}</a> --}}
+                    <li class="breadcrumb-item"><a href="{{ $breadcrumb->url }}">{{ strtoupper($breadcrumb->title) }}</a></li>
                 @else
-                    <li class="breadcrumb-item active">{{ $breadcrumb->title }}</li>
-                    {{-- <li class="nav-link disabled">{{ $breadcrumb->title }}</li> --}}
+                    <li class="breadcrumb-item active">{{ strtoupper($breadcrumb->title) }}</li>
                 @endif
 
             @endforeach
-        {{-- </li> --}}
         </ol>
     @endif
 @endif
