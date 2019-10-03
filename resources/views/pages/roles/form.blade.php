b0159f59f65d04f4ae433632e408954f4cc5b85c
kodokok <sabdo.tri.atmojo@gmail.com>
2019-10-03 23:58:12 +0700

    move roles view

---
+++ resources/views/pages/roles/form.blade.php
@@ -0,0 +1,21 @@
+{!! Form::open([
+    'route' => 'roles.store',
+    'method' => 'POST',
+]) !!}
+
+<div class="form-group row">
+    <label for="name" class="col-sm-2 col-form-label">Name</label>
+    <div class="col-sm-10">
+        {!! Form::text('name', null, ['class' => 'form-control', 'id' => 'name']) !!}
+        <div id="error-name" class="invalid-feedback"></div>
+    </div>
+</div>
+<div class="form-group row">
+    <label for="guard_name" class="col-sm-2 col-form-label">Guard</label>
+    <div class="col-sm-10">
+        {!! Form::select('guard_name', $guards, null, ['class' => 'form-control', 'id' => 'guard_name']) !!}
+        <div id="error-guard_name" class="invalid-feedback"></div>
+    </div>
+</div>
+
+{!! Form::close() !!}
