9d8551433c7250a15162713b0fe64f82f1f629f0
kodokok <sabdo.tri.atmojo@gmail.com>
2019-10-22 22:23:23 +0700

    Update action.blade.php

--- resources/views/pages/auditplan/action.blade.php
+++ resources/views/pages/auditplan/action.blade.php
@@ -6,8 +6,8 @@
     <a href="{{ $url_approve }}" class="btn btn-sm btn-warning btn-approve" title="Approved: {{ $model->id }}"><i class="far fa-thumbs-up"></i></a></a>
 @endif
 @hasanyrole('admin|auditor_lead|auditor')
-    <a href="{{ $url_destroy }}" class="btn btn-danger btn-sm btn-delete" title="{{ $model->id }}"><i class="fas fa-trash"></i></a></a>
     <a href="{{ $url_edit }}" class="btn btn-sm btn-primary edit" title="Edit: {{ $model->id }}"><i class="fas fa-pencil-alt"></i></a></a>
+    <a href="{{ $url_destroy }}" class="btn btn-danger btn-sm btn-delete" title="{{ $model->id }}"><i class="fas fa-trash"></i></a></a>
 @else
     @hasanyrole('kadept')
         <a href="{{ $url_edit }}" class="btn btn-sm btn-primary edit" title="Edit: {{ $model->id }}"><i class="fas fa-pencil-alt"></i></a></a>
