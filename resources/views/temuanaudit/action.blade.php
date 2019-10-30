867e16e9a6bab85218c778fb1755d4ba1cdd0fa4
kodokok <sabdo.tri.atmojo@gmail.com>
2019-10-31 02:57:06 +0700

    delete temporary temuanaudit view

--- resources/views/temuanaudit/action.blade.php
+++
@@ -1,3 +0,0 @@
-<a href="{{ $url_edit }}" class="btn btn-sm btn-primary edit" title="Edit: {{ $model->id }}"><i class="fas fa-pencil-alt"></i></a></a>
-<a href="{{ $url_destroy }}" class="btn btn-danger btn-sm btn-delete" title="{{ $model->id }}"><i class="fas fa-trash"></i></a></a>
-
